2da7ff19b08913efa510f754082b01a24162835b
ravilkondakov <ravilkondako@mail.ru>
2022-02-15 15:58:13 +0300

    fix

--- test/test.py
+++ test/test.py
@@ -4,20 +4,28 @@ from run import api_parser
 
 
 class TestApiFunc(unittest.TestCase):
-    url_current = 'https://api.thedogapi.com/v1/breeds/1'
-    url_wrong = 'https://www.youtube.com/'
 
-    def test_api_parse_current(self):
-        response, request = api_parser(self.url_current)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.headers.get("Content-Type"), 'application/json; charset=utf-8')
-        self.assertEqual(dict, type(request))
+    def setUp(self) -> None:
+        self.url_current = 'https://api.thedogapi.com/v1/breeds/1'
+        self.response_current, self.request_current = api_parser(self.url_current)
+        self.url_wrong = 'https://www.youtube.com/'
+        self.response_uncurrent, self.request_uncurrent = api_parser(self.url_wrong)
 
-    def test_api_parse_uncurrent(self):
-        response, request = api_parser(self.url_wrong)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.headers.get("Content-Type"), 'text/html; charset=utf-8')
+    def test_api_parse_status_code_200(self):
+        self.assertEqual(self.response_current.status_code, 200)
+
+    def test_api_parse_headers(self):
+        self.assertEqual(self.response_current.headers.get("Content-Type"), 'application/json; charset=utf-8')
+
+    def test_api_parse_json(self):
+        self.assertEqual(dict, type(self.request_current))
+
+    def test_api_parse_uncurrent_status_code(self):
+        self.assertEqual(self.response_uncurrent.status_code, 200)
+
+    def test_api_parse_uncurrent_content_type(self):
+        self.assertEqual(self.response_uncurrent.headers.get("Content-Type"), 'text/html; charset=utf-8')
+
 
 if __name__ == '__main__':
     unittest.main()
-
